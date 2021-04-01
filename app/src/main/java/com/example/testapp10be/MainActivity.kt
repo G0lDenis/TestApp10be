5f242c9ef06d069fb236b6a1c891cf77b21e999b
Fesmat <dden4929@gmail.com>
2021-04-01 22:39:37 +0300

    Two activities added.

---
+++ app/src/main/java/com/example/testapp10be/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.testapp10be
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
