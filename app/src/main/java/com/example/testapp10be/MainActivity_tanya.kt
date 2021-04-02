812629f13283929377f29e98ebd79d3bf3b0d627
tdnikolaeva <tdnikolaeva@mail.ru>
2021-04-03 00:58:28 +0300

    added activity with the first quatrain

---
+++ app/src/main/java/com/example/testapp10be/MainActivity_tanya.kt
@@ -0,0 +1,13 @@
+package com.example.testapp10be
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication2.R
+
+class MainActivity_tanya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_t)
+    }
+
+}
